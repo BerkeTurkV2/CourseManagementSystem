f26ad147a6d61c78cea400639839d69275158e50
Berke <forcoding2022v2@gmail.com>
2024-12-21 15:31:38 +0300

    Sidebar konum ayarlamaları

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaHome, FaUserGraduate, FaChalkboardTeacher, FaUserPlus } from 'react-icons/fa';
+import { FaHome, FaUserGraduate, FaUser, FaCalendarAlt , FaRunning, FaChalkboardTeacher, FaUserPlus } from 'react-icons/fa';
 import { PiNoteFill, PiNotePencilBold } from "react-icons/pi";
 import { BsCalendarWeek } from 'react-icons/bs';
 import { Link } from "react-router-dom";
@@ -10,37 +10,37 @@ const Sidebar = ({ userType }) => {
         admin: {
             title: 'Admin Paneli',
             menuItems: [
-                { name: 'Dashboard', path: '/admin/mainPanel', icon: FaHome },
-                { name: 'Takvim', path: '/admin/calendar', icon: BsCalendarWeek },
-                { name: 'Öğrenciler', path: '/admin/studentsList', icon: FaUserGraduate },
-                { name: 'Öğretmenler', path: '/admin/teachersList', icon: FaChalkboardTeacher },
-                { name: 'Sınavlar', path: '/admin/examsList', icon: PiNoteFill },
-                { name: 'Yeni kullanıcı', path: '/admin/newUser', icon: FaUserPlus },
-                { name: 'Yeni sonuçlar', path: '/admin/newExam', icon: PiNotePencilBold },
+                { name: 'Dashboard', path: '/admin/mainPanel',style: 'nav-item mb-4' ,icon: FaHome },
+                { name: 'Takvim', path: '/admin/calendar',style: 'nav-item mb-2', icon: BsCalendarWeek },
+                { name: 'Öğrenciler', path: '/admin/studentsList',style: 'nav-item mb-2', icon: FaUserGraduate },
+                { name: 'Öğretmenler', path: '/admin/teachersList',style: 'nav-item mb-2', icon: FaChalkboardTeacher },
+                { name: 'Sınavlar', path: '/admin/examsList',style: 'nav-item mb-4', icon: PiNoteFill },
+                { name: 'Yeni kullanıcı', path: '/admin/newUser',style: 'nav-item mb-2', icon: FaUserPlus },
+                { name: 'Yeni sonuçlar', path: '/admin/newExam',style: 'nav-item mb-2', icon: PiNotePencilBold },
             ]
         },
         student: {
             title: 'Öğrenci Paneli',
             menuItems: [
-                { name: 'Dashboard', path: '/student/mainPanel', icon: FaHome },
-                { name: 'Takvim', path: '/student/calendar', icon: BsCalendarWeek },
-                { name: 'Sonuçlarım', path: '/student/examResults', icon: PiNoteFill },
-                { name: 'Randevularım', path: '/student/appointments', icon: BsCalendarWeek },
-                { name: 'Ödevlerim', path: '/student/homeworks', icon: PiNotePencilBold },
-                { name: 'Devamsızlığım', path: '/student/attendance', icon: FaUserGraduate },
-                { name: 'Profil', path: '/student/profile', icon: FaUserGraduate }
+                { name: 'Dashboard', path: '/student/mainPanel', style: 'nav-item mb-4', icon: FaHome },
+                { name: 'Takvim', path: '/student/calendar', style: 'nav-item mb-2', icon: BsCalendarWeek },
+                { name: 'Sonuçlarım', path: '/student/examResults', style: 'nav-item mb-2', icon: PiNoteFill },
+                { name: 'Ödevlerim', path: '/student/homeworks', style: 'nav-item mb-2', icon: PiNotePencilBold },
+                { name: 'Randevularım', path: '/student/appointments', style: 'nav-item mb-2', icon: FaCalendarAlt },
+                { name: 'Devamsızlığım', path: '/student/attendance', style: 'nav-item mb-4', icon: FaRunning },
+                { name: 'Profil', path: '/student/profile', style: 'nav-item mb-2', icon: FaUser }
             ]
         },
         teacher: {
             title: 'Öğretmen Paneli',
             menuItems: [
-                { name: 'Dashboard', path: '/teacher/mainPanel', icon: FaHome },
-                { name: 'Takvim', path: '/teacher/calendar', icon: BsCalendarWeek },
-                { name: 'Öğrencilerim', path: '/teacher/students', icon: FaUserGraduate },
-                { name: 'Sınavlar', path: '/teacher/exams', icon: PiNoteFill },
-                { name: 'Ödevler', path: '/teacher/homeworks', icon: PiNotePencilBold },
-                { name: 'Randevular', path: '/teacher/appointments', icon: BsCalendarWeek },
-                { name: 'Profil', path: '/teacher/profile', icon: FaChalkboardTeacher }
+                { name: 'Dashboard', path: '/teacher/mainPanel',style: 'nav-item mb-2', icon: FaHome },
+                { name: 'Takvim', path: '/teacher/calendar',style: 'nav-item mb-2', icon: BsCalendarWeek },
+                { name: 'Öğrencilerim', path: '/teacher/students',style: 'nav-item mb-2', icon: FaUserGraduate },
+                { name: 'Sınavlar', path: '/teacher/exams',style: 'nav-item mb-2', icon: PiNoteFill },
+                { name: 'Ödevler', path: '/teacher/homeworks',style: 'nav-item mb-2', icon: PiNotePencilBold },
+                { name: 'Randevular', path: '/teacher/appointments',style: 'nav-item mb-2', icon: BsCalendarWeek },
+                { name: 'Profil', path: '/teacher/profile',style: 'nav-item mb-2', icon: FaChalkboardTeacher }
             ]
         }
     };
@@ -52,11 +52,11 @@ const Sidebar = ({ userType }) => {
             <div className="text-center mb-4 mt-3">
                 <p>EGE-X Kurs Merkezi</p>
                 <img src="/images/logo2.png" alt="Logo" className="rounded-circle logo" />
-                <h5 className="text-center mb-2">{config.title}</h5>
+                <h5 className="text-center mb-4">{config.title}</h5>
             </div>
             <ul className="nav flex-column">
                 {config.menuItems.map((item, index) => (
-                    <li className="nav-item mb-2" key={index}>
+                    <li className={item.style} key={index}>
                         <Link to={item.path} className="nav-link text-white d-flex align-items-center">
                             <item.icon className="me-3 fs-3" /> {item.name}
                         </Link>
